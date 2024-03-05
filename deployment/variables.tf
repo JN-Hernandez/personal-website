fc630d9efa7543f3d597118ee0c413e7d2116a50
JN Hernández <jes.nat.hernandez@gmail.com>
2024-03-04 19:44:34 -0700

    Create S3 bucket
    Creating the S3 bucket for our static website.

---
+++ deployment/variables.tf
@@ -0,0 +1,19 @@
+variable "account_id" {
+  default = "123456789012"
+  type    = string
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+  type    = string
+}
+
+variable "owner_name" {
+  default = "jn-hernandez"
+  type    = string
+}
+
+variable "project" {
+  default = "personal-website"
+  type    = string
+}
